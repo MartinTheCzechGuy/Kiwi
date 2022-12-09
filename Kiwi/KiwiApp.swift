7ae72f8a1127aedbbb474187d1d05b0dc4052cfa
Martin Kolomazník <kolomaznik.martin@outlook.com>
2022-12-09 13:16:48 +0100

    Initial Commit

---
+++ Kiwi/KiwiApp.swift
@@ -0,0 +1,17 @@
+//
+//  KiwiApp.swift
+//  Kiwi
+//
+//  Created by Martin on 09.12.2022.
+//
+
+import SwiftUI
+
+@main
+struct KiwiApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
